681eabcef598c55093cc842b21317eebb00802fa
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-01-13 22:52:02 +0530

    hcf found

--- src/HCF.java
+++ src/HCF.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
 public class HCF {
     
         // TODO: Read two integers and print their HCF
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
+            while(num2%num1 != 0)
+            {
+                int temp = num1;
+                num1=num2%num1;
+                num2=num1;
+                }
+             System.out.println(num1);
+        }
     
 }
