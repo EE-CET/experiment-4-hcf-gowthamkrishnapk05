7b883a14b4736dedf71a9d027eecba4363c2a9b8
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-01-13 23:04:49 +0530

    updated

--- src/HCF.java
+++ src/HCF.java
@@ -6,7 +6,7 @@ public class HCF {
             Scanner sc = new Scanner(System.in);
             int num1 = sc.nextInt();
             int num2 = sc.nextInt();
-            while(num1 != 0 && num2%num1 != 0)
+            while(num1 != 0 )
             {
                 int temp = num1;
                 num1=num2%num1;
