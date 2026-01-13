979fcc950c7ee09342fb36a6154f6a82f8b06fae
Gowtham Krishna P K <gowthamkrishnapk05@gmail.com>
2026-01-13 23:01:44 +0530

    error rectified

--- src/HCF.java
+++ src/HCF.java
@@ -6,11 +6,11 @@ public class HCF {
             Scanner sc = new Scanner(System.in);
             int num1 = sc.nextInt();
             int num2 = sc.nextInt();
-            while(num2%num1 != 0)
+            while(num1 != 0 && num2%num1 != 0)
             {
                 int temp = num1;
                 num1=num2%num1;
-                num2=num1;
+                num2=temp;
                 }
              System.out.println(num1);
         }
